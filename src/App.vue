f8924dd695aa2e6307fe72e42ca1c710d5ff0d63
Shashwat Tripathi <stripathix@gmail.com>
2019-07-03 10:12:07 +0530

    FIrst draft

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,58 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld :track="track"/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-
+var a = null;
 export default {
   name: 'app',
   components: {
     HelloWorld
+  },
+  data() {
+    return {
+      tracks: {
+        pointer: {
+          x: 1,
+          y: 1,
+        },
+        video: null,
+      }
+    }
+  },
+  computed: {
+    track() {
+      return this.tracks;
+    }
+  },
+  mounted() {
+    setInterval(() => {
+      this.track.pointer.x += 1.1;
+      // this.track.video = a;
+    }, 100);
+    const videoPlayer = document.getElementById("video-player") // this is a <video> element
+
+function onSuccessWithSrcObject(stream) {
+   videoPlayer.srcObject = stream;
+}
+
+function onSuccessWithSrc(stream) {
+   videoPlayer.src = window.URL.createObjectURL(stream);
+}
+
+navigator.mediaDevices.getUserMedia({ audio: true, video: true })
+  .then(stream => {
+    // if ("srcObject" in videoPlayer) {
+    //   onSuccessWithSrcObject(stream)
+    // } else {
+    //   onSuccessWithSrc(stream)
+    // }
+    // a = stream;
+    this.tracks.video = stream;
+  })
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,31 +1,10 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div >
+    {{t}}
+    <video
+      autoplay="autoplay"
+      :srcObject.prop="trackStream"
+      style="height: 300px; width: 300px" />
   </div>
 </template>
 
@@ -33,7 +12,39 @@
 export default {
   name: 'HelloWorld',
   props: {
-    msg: String
+    track: Object,
+  },
+  watch: {
+    // whenever question changes, this function will run
+    'track': {
+      deep: true,
+          handler (val, oldVal) {
+            // do your stuff
+            console.log("hbjk");
+          }
+    }
+  },
+  data() {
+    return {
+      t: 1,
+    }
+  },
+  mounted() {
+    setInterval(() => {
+      this.t += 1;
+    }, 3000)
+  },
+  computed: {
+    trackStream() {
+      // console.log("trackStream");
+      var a = this.track ? this.track.video : new MediaStream();
+      console.log(a);
+      return a;
+    },
+    pointer() {
+      // console.log("pointer");
+      return this.track.pointer.x;// + this.track.video.id;
+    }
   }
 }
 </script>
